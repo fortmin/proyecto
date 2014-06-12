64add4e9217b9fbb19b86007d1b7f71d6ffeeb76
Fernando Miños <fernandominos@gmail.com>
2014-06-12 19:40:24 -0300

    Agregue funciones para saber si el móvil tiene NFC y Bluetooth

--- ProshopAPI/src/com/fortmin/proshopapi/ble/ProShopBleMgr.java
+++ ProshopAPI/src/com/fortmin/proshopapi/ble/ProShopBleMgr.java
@@ -1,5 +1,18 @@
 package com.fortmin.proshopapi.ble;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+
 public class ProShopBleMgr {
+	
+	public boolean soportaBLE(Context context) {
+		PackageManager pckMgr = context.getPackageManager();
+		return pckMgr.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
+	}
+
+	public boolean soportaBluetooth(Context context) {
+		PackageManager pckMgr = context.getPackageManager();
+		return pckMgr.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+	}
 
 }
--- ProshopAPI/src/com/fortmin/proshopapi/nfc/ProShopNFCMgr.java
+++ ProshopAPI/src/com/fortmin/proshopapi/nfc/ProShopNFCMgr.java
@@ -1,5 +1,18 @@
 package com.fortmin.proshopapi.nfc;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+
 public class ProShopNFCMgr {
+
+	public boolean soportaNFC(Context context) {
+		PackageManager pckMgr = context.getPackageManager();
+		return pckMgr.hasSystemFeature(PackageManager.FEATURE_NFC);
+	}
+
+	public boolean soportaNFCHce(Context context) {
+		PackageManager pckMgr = context.getPackageManager();
+		return pckMgr.hasSystemFeature(PackageManager.FEATURE_NFC_HOST_CARD_EMULATION);
+	}
 
 }
