79d133b3752f8029f926b8f880a72656885c8d85
Jafortti <Jafortti@JulioFortti>
2014-06-12 17:17:58 -0300

    cambio en el main extend, cambie por activity, y el style

--- ProshopAPI/src/com/fortmin/proshopapi/MainActivity.java
+++ ProshopAPI/src/com/fortmin/proshopapi/MainActivity.java
@@ -1,26 +1,22 @@
 package com.fortmin.proshopapi;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
 	}
 
 	@Override
