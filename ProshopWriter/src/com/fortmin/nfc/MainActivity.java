2b85944487d87bf56bff638a51a825e7e4250416
Fernando Miños <fernandominos@gmail.com>
2014-06-24 00:06:49 -0300

    Nueva version del ProshopWriter de Julio

--- ProshopWriter/src/com/fortmin/nfc/MainActivity.java
+++ ProshopWriter/src/com/fortmin/nfc/MainActivity.java
@@ -3,9 +3,12 @@ package com.fortmin.nfc;
 import java.net.URISyntaxException;
 
 import com.fortmin.proshopapi.ProShopMgr;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+
+import android.graphics.Typeface;
 import android.nfc.NdefMessage;
 import android.nfc.Tag;
 import android.os.Bundle;
@@ -29,41 +32,34 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final EditText editUrl = (EditText) findViewById(R.id.Texturl);
+		Button writeItemButton = (Button) findViewById(R.id.botonWrite);
+		// cambio la fuente del botón
+		Typeface fuente = Typeface.createFromAsset(getAssets(),
+				"gloriahallelujah.ttf");
+		// agrego la fuente al botón
+		writeItemButton.setTypeface(fuente);
+		// Le pongo degrade plateado al botón
+		writeItemButton.setBackgroundResource(R.drawable.degradado);
 
-		Button botonWriteUrl = (Button) findViewById(R.id.botonWriteUrl);
-		Button botonWriteMail = (Button) findViewById(R.id.botonWriteMail);
-		Button botonWriteTel = (Button) findViewById(R.id.botonWriteTel);
+		writeItemButton.setOnClickListener(new View.OnClickListener()
 
-		botonWriteUrl.setOnClickListener(new View.OnClickListener() {
+		{
 			public void onClick(View view) {
-				textoUrl = editUrl.getText().toString();
-				Mensaje(view, "Toque el Tag NFC Tag para grabar\n");
+
+				mostrarOpciones();
 			}
 		});
-		
-		botonWriteMail.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				textoUrl = editUrl.getText().toString();
-				Mensaje(view, "Toque el Tag NFC Tag para grabar\n");
-			}
-		});
-		
-		botonWriteTel.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				textoUrl = editUrl.getText().toString();
-				Mensaje(view, "Toque el Tag NFC Tag para grabar\n");
-			}
-		});
-
-		context = getApplicationContext();
+		// context=getApplicationContext();
 		Button salirButton = (Button) this.findViewById(R.id.botonSalir);
+		// agrego la fuente al botón
+		salirButton.setTypeface(fuente);
+		// Le pongo degrade plateado al botón
+		salirButton.setBackgroundResource(R.drawable.degradado);
 		salirButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				finish();
 			}
 		});
-		proshopmgr = new ProShopMgr(context);
 
 	}
 
@@ -97,32 +93,9 @@ public class MainActivity extends Activity {
 		toast.show();
 	}
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		proshopmgr.escucharTagNdefGrabar(this, context, getClass());
-	}
-
-	@Override
-	public void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
-		tag = proshopmgr.obtenerTagDescubierto(intent);
-		if (tag != null) {
-			try {
-				NdefMessage newMessage = proshopmgr
-						.prepararMensNdefUrl(textoUrl);
-				proshopmgr.escribirNdefMessageToTag(newMessage, tag);
-			} catch (URISyntaxException e) {
-				Mensaje(this, "URL con formato incorrecto");
-			}
-		}
-
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		proshopmgr.noEscucharTagNdefGrabar(this, context);
+	private void mostrarOpciones() {
+		Intent opciones = new Intent(this, Opciones.class);
+		startActivity(opciones);
 	}
 
 }
